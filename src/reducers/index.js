f04d9752bbbe147e8bcd3848da092420cae50683
Pob <590650@gmail.com>
2018-03-30 01:05:40 +0700

    finish this site

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
+import BooksReducer from './reducer_books'
+import AciveBook from './reducer_active_book'
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer,
+  activeBook: AciveBook
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'Javascript: The good part', pages: 57},
+    {title: 'The lord of the ring', pages: 88},
+    {title: 'Harry Potter', pages: 74},
+    {title: 'Gone Girel', pages: 2}
+  ]
+}
